27bd3d8377bdd53a0d7dcc9e2653ade2334e4163
leonkae <leon29kae@gmail.com>
2023-01-25 22:14:52 +0300

    modify:admin file

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Item, Order, OrderItem
 
 # Register your models here.
+class ItemAdmin(admin.ModelAdmin):
+    prepopulated_fields ={'slug': ('title')}
+    list_display =[
+        'title',
+        'price',
+        'discount_price'
+    ]
+    
+    
+admin.site.register(Item)
+admin.site.register(Order)
+admin.site.register(OrderItem)   
